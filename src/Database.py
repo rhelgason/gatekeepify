0e2f8659c887e8d839e311174866dcc1f74c561d
Ryan Helgason <helgasonryan@fb.com>
2024-11-26 18:39:51 -0500

    Set up basic relational db

---
+++ src/Database.py
@@ -0,0 +1,79 @@
+import os
+import sqlite3
+
+DB_DIRECTORY = "db"
+DB_NAME = "database.db"
+
+
+class Database:
+    def __init__(self, db_name=DB_NAME):
+        path = os.path.join(DB_DIRECTORY, db_name)
+        self.conn = sqlite3.connect(path)
+        self.cursor = self.conn.cursor()
+
+    def __create_tables(self):
+        self.__create_dim_all_tracks()
+        self.__create_dim_all_artists()
+        self.__create_dim_track_to_artist()
+        self.__create_dim_all_users()
+        self.__create_dim_all_listens()
+
+    # table for storing information about every track
+    def __create_dim_all_tracks(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS dim_all_tracks (
+            track_id VARCHAR(255),
+            track_name VARCHAR(255),
+            album_name VARCHAR(255)
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    # table for storing information about every artist
+    def __create_dim_all_artists(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS dim_all_artists (
+            artist_id VARCHAR(255),
+            artist_name VARCHAR(255)
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    # mapping table between tracks and artists
+    def __create_dim_track_to_artist(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS dim_track_to_artist (
+            track_id VARCHAR(255),
+            artist_id VARCHAR(255)
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    # table for storing information about every user
+    def __create_dim_all_users(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS dim_all_users (
+            user_id VARCHAR(255),
+            user_name VARCHAR(255)
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    # table that stores every track listened to by every user
+    def __create_dim_all_listens(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS dim_all_listens (
+            user_id VARCHAR(255),
+            track_id VARCHAR(255),
+            ds DATE
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
