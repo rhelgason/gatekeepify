5efd2c55c2288a0006699f3b4cfda75473c95e58
Ryan Helgason <helgasonryan@fb.com>
2024-11-28 12:48:17 -0500

    Finish simple cron job backfill

--- src/cron.py
+++ src/cron.py
@@ -9,5 +9,7 @@ client = SpotifyClient()
 user = client.gen_current_user()
 most_recent_ts = db.gen_most_recent_listen_time(user)
 recent_tracks = client.gen_most_recent_tracks(most_recent_ts)
+print(f"Got {len(recent_tracks)} recent tracks from Spotify API for user ID {user.name}.")
 
-# TODO: upsert all new tracks to database
+# upsert all new tracks to database
+# db.upsert_all_tables(user, recent_tracks)
