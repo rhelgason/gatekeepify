dde670dc22b536a04c5c18df1375de954347c001
Ryan Helgason <helgasonryan@fb.com>
2024-11-27 19:14:51 -0500

    Parent class architecture

--- src/spotify/types.py
+++ src/spotify/types.py
@@ -1,4 +1,4 @@
-class User:
+class Artifact:
     id: str
     name: str
 
@@ -6,6 +6,20 @@ class User:
         self.id = id
         self.name = name
     
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data['id'],
+            data['name'],
+        )
+    
+    def __hash__(self) -> int:
+        return hash(self.id)
+    
+    def __eq__(self, other) -> bool:
+        return self.id == other.id and self.name == other.name
+
+class User(Artifact):
     @classmethod
     def from_dict(cls, data):
         return cls(
@@ -13,39 +27,13 @@ class User:
             data['display_name'],
         )
 
-class Album:
-    id: str
-    name: str
+class Album(Artifact):
+    pass
 
-    def __init__(self, id, name) -> None:
-        self.id = id
-        self.name = name
-    
-    @classmethod
-    def from_dict(cls, data):
-        return cls(
-            data['id'],
-            data['name'],
-        )
+class Artist(Artifact):
+    pass
 
-class Artist:
-    id: str
-    name: str
-
-    def __init__(self, id, name) -> None:
-        self.id = id
-        self.name = name
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(
-            data['id'],
-            data['name'],
-        )
-
-class Track:
-    id: str
-    name: str
+class Track(Artifact):
     album: Album
     artists: list[Artist]
 
@@ -63,3 +51,6 @@ class Track:
             Album.from_dict(data['album']),
             [Artist.from_dict(artist) for artist in data['artists']]
         )
+    
+    def __eq__(self, other) -> bool:
+        return super().__eq__(other) and self.album == other.album and set(self.artists) == set(other.artists)
