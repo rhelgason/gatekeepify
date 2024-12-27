764d7ef4d6a6654fb2f7fc93857de87afa87ddac
Ryan Helgason <helgasonryan@fb.com>
2024-12-27 15:28:58 -0600

    Even cleaner constants testing

--- tests/test_spotify_client.py
+++ tests/test_spotify_client.py
@@ -10,20 +10,22 @@ from unittest.mock import patch
 CLIENT_ID = "test_id"
 CLIENT_SECRET = "test_secret"
 
+@patch("builtins.input", side_effect=[CLIENT_ID, CLIENT_SECRET])
 class TestSpotifyClient(unittest.TestCase):
-    @patch("builtins.input", side_effect=[CLIENT_ID, CLIENT_SECRET])
+    def setUp(self) -> None:
+        self.path = ".".join((HOST_CONSTANTS_TEST_PATH, "py"))
+
     def test_get_host_client(self, mock_input) -> None:
         # delete test constants, if exists
-        path = ".".join((HOST_CONSTANTS_TEST_PATH, "py"))
         try:
-            os.remove(path)
+            os.remove(self.path)
         except OSError:
             pass
-        self.assertEqual(pl.Path(path).resolve().is_file(), False)
+        self.assertEqual(pl.Path(self.path).resolve().is_file(), False)
         
         SpotifyClient(is_test=True)
         self.assertEqual(mock_input.call_count, 2)
-        self.assertEqual(pl.Path(path).resolve().is_file(), True)
+        self.assertEqual(pl.Path(self.path).resolve().is_file(), True)
         host_constants_spec = __import__(
             HOST_CONSTANTS_TEST_PATH.split("/")[-1].removesuffix(".py"),
             globals(),
@@ -33,4 +35,6 @@ class TestSpotifyClient(unittest.TestCase):
         )
         self.assertEqual(host_constants_spec.CLIENT_ID, CLIENT_ID)
         self.assertEqual(host_constants_spec.CLIENT_SECRET, CLIENT_SECRET)
-        os.remove(path)
+
+    def tearDown(self) -> None:
+        os.remove(self.path)
